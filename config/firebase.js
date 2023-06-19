9246c6424dc625c4d76abbe0307ac76713b973cb
Subhadip <mnhacker2001@gmail.com>
2023-06-20 03:14:25 +0530

    fix pass

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,10 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey-abhay-sah.json");
+require("dotenv").config({ path: "config/config.env" });
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://abheyshahpersonalproject-default-rtdb.firebaseio.com",
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 
 module.exports = admin;
